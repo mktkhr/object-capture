47f9ef5c660ddab165a876992e908f2c88313e7a
EMS engineering <j39g8bxh@gmail.com>
2024-07-07 22:53:32 +0900

    [add] #1 新規プロジェクトを作成

---
+++ object-capture/object_captureApp.swift
@@ -0,0 +1,17 @@
+//
+//  object_captureApp.swift
+//  object-capture
+//
+//  Created by Takahiro Maki on 2024/07/07.
+//
+
+import SwiftUI
+
+@main
+struct object_captureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
